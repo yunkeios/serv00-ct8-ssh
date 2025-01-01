26f2453c9299bd8f25e759395addcdac0c175379
yunkeios <lyk6668869@gmail.com>
2025-01-01 17:55:39 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -39,6 +39,10 @@ elif [ -e "${WORKDIR}/argo.sh" ]; then
   green "正在添加 argo 的 crontab 重启任务"
   NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) && cd ${WORKDIR} && ${CRON_ARGO}\n"
   NEW_CRONTAB+="*/10 * * * * pgrep -x \"bot\" > /dev/null || cd ${WORKDIR} && ${CRON_ARGO}\n"
+elif [ -e "${WORKDIR}/web" ]; then
+  green "正在添加 singbox 的 crontab 重启任务"
+  NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) cd ${WORKDIR} && ${CRON_SB}\n"
+  NEW_CRONTAB+="* * * * * bash sb00.sh || 5 \n"
 fi
 
 # 将 crontab 任务更新一次性添加
